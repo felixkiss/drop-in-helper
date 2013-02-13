0592e0d849759913ab6738793c7c487dab4cc022
Felix Kiss <f.kiss@me.com>
2013-02-13 20:02:21 +0100

    Force SSL

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module DropInRails
   class Application < Rails::Application
-  	# config.force_ssl = true
+  	config.force_ssl = true
   	
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
