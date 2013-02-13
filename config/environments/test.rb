4d61085e3c0369a108bc37d1552dfb53e17e9aa7
Felix Kiss <f.kiss@me.com>
2013-02-13 21:15:54 +0100

    Configure dev machine to reflect production environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,9 @@
 DropInRails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
